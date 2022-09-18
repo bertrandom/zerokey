4999c20b62c64be47241affc7a4354b03f44a770
Bertrand Fan <bertrand@fan.net>
2022-09-18 16:48:29 -0700

    bedroom config

--- app.js
+++ app.js
@@ -9,42 +9,43 @@ console.log('zerokey started');
 keyboard.on('keypress', (e) => {
     if (e.value === 1) {
         switch (e.code) {
-            case 71:
+            case 2:
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/36/setLevel/100?access_token=${config.hubitat.access_token}`);
                 console.log('turn on lights');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/7/setLevel/100?access_token=${config.hubitat.access_token}`);
                 break;
-            case 72:
+            case 3:
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/36/setLevel/25?access_token=${config.hubitat.access_token}`);
                 console.log('set lights to 25%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/7/setLevel/25?access_token=${config.hubitat.access_token}`);
                 break;
-            case 73:
+            case 4:
                 console.log('turn off lights');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/7/off?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/36/off?access_token=${config.hubitat.access_token}`);
                 break;
-            case 75:
+            case 5:
                 console.log('open blinds');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/8/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/open?access_token=${config.hubitat.access_token}`);
                 break;
-            case 76:
+            case 6:
                 console.log('open blinds to 50%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/8/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/setLevel/50?access_token=${config.hubitat.access_token}`);
                 break;
-            case 77:
+            case 7:
                 console.log('close blinds');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/8/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/close?access_token=${config.hubitat.access_token}`);
                 break;
-            case 79:
-                console.log('7');
-                break;
-            case 80:
-                console.log('8');
-                break;
-            case 81:
-                console.log('9');
-                break;
-            case 82:
-                console.log('0');
-                break;
+
         }
     }
 });
