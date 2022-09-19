3eeaa0e65ad2822a0837f352367930e21700bcc1
Bertrand Fan <bertrand@fan.net>
2022-09-18 23:31:41 -0700

    Use hubitat group

--- app.js
+++ app.js
@@ -23,27 +23,15 @@ keyboard.on('keypress', (e) => {
                 break;
             case 5:
                 console.log('open blinds');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/open?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/open?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/open?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/open?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/open?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/74/setLevel/100?access_token=${config.hubitat.access_token}`);
                 break;
             case 6:
                 console.log('open blinds to 50%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/setLevel/50?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/setLevel/50?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/setLevel/50?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/setLevel/50?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/setLevel/50?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/74/setLevel/50?access_token=${config.hubitat.access_token}`);
                 break;
             case 7:
                 console.log('close blinds');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/68/close?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/69/close?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/70/close?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/71/close?access_token=${config.hubitat.access_token}`);
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/72/close?access_token=${config.hubitat.access_token}`);
+                rp(`http://${config.hubitat.host}/apps/api/88/devices/74/setLevel/0?access_token=${config.hubitat.access_token}`);
                 break;
 
         }
