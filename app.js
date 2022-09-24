b22e43a36dfca69b4aed71e8a97dbb3d39b3bb8f
Bertrand Fan <bertrand@fan.net>
2022-09-24 08:22:53 -0700

    Use functions, fix expires bug

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const getAccessToken = async () => {
             const body = JSON.parse(response);
 
             storedAccessToken = body;
-            storedAccessToken.expires = now + storedAccessToken.expires;
+            storedAccessToken.expires = now + storedAccessToken.expires_in;
 
             return storedAccessToken.access_token;
         }
@@ -42,6 +42,67 @@ const getAccessToken = async () => {
     }
 }
 
+const switchToRecordPlayer = async () => {
+
+    accessToken = await getAccessToken();
+
+    response = await rp({
+        uri: "https://api.ws.sonos.com/control/api/v1/groups/RINCON_48A6B8033D6501400:2733352432/playback/lineIn",
+        method: "POST",
+        json: true,
+        body: {
+            "deviceId": "RINCON_48A6B8214BA201400"
+        },
+        headers: {
+            "Authorization": "Bearer " + accessToken,
+        },
+        resolveWithFullResponse: true
+    });
+
+    if (response.statusCode == 200) {
+        console.log('Successfully switched input to record player.');
+    }
+
+    response = await rp({
+        uri: "https://api.ws.sonos.com/control/api/v1/groups/RINCON_48A6B8033D6501400:2733352432/playback/play",
+        method: "POST",
+        json: true,
+        body: {
+            "deviceId": "RINCON_48A6B8214BA201400"
+        },
+        headers: {
+            "Authorization": "Bearer " + accessToken,
+        },
+        resolveWithFullResponse: true
+    });
+
+    if (response.statusCode == 200) {
+        console.log('Successfully started playing record player.');
+    }
+
+}
+
+const switchToTV = async () => {
+
+    accessToken = await getAccessToken();
+
+    response = await rp({
+        uri: "https://api.ws.sonos.com/control/api/v1/players/RINCON_48A6B8033D6501400/homeTheater",
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer " + accessToken,
+            "Content-Type": "application/json"
+        },
+        body: "{}",
+        resolveWithFullResponse: true
+    });
+
+    if (response.statusCode == 200) {
+        console.log('Successfully switched input to TV.');
+    }
+
+}
+
 keyboard.on('keypress', async (e) => {
 
     let accessToken = null;
@@ -51,38 +112,13 @@ keyboard.on('keypress', async (e) => {
 
         switch (e.code) {
             case 2:
-
                 console.log('switch sonos input to record player');
-
-                accessToken = await getAccessToken();
-                response = await rp({
-                    uri: "https://api.ws.sonos.com/control/api/v1/groups/RINCON_48A6B8033D6501400:2733352432/playback/lineIn",
-                    method: "POST",
-                    json: true,
-                    body: {
-                        "deviceId": "RINCON_48A6B8214BA201400"
-                    },
-                    headers: {
-                        "Authorization": "Bearer " + accessToken,
-                    }
-                });
-                console.log(response);
+                await switchToRecordPlayer();
                 break;
 
             case 3:
                 console.log('switch sonos input to TV');                
-
-                accessToken = await getAccessToken();
-                response = await rp({
-                    uri: "https://api.ws.sonos.com/control/api/v1/players/RINCON_48A6B8033D6501400/homeTheater",
-                    method: "POST",
-                    headers: {
-                        "Authorization": "Bearer " + accessToken,
-                        "Content-Type": "application/json"
-                    },
-                    body: "{}",
-                });
-                console.log(response);
+                await switchToTV();
                 break;
         }
 
