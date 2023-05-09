47e2fcb0db80b9146a6016404ecfc4ab3c22ce12
Bertrand Fan <bertrand@fan.net>
2023-05-08 19:38:31 -0700

    bedroom - iris nightstand

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ console.log('zerokey started');
 
 keyboard.on('keypress', async (e) => {
     if (e.value === 1) {
+
+        let response = null;
+
+        console.log(e.code);
+
         switch (e.code) {
             case 2:
 
-                const response = await rp({
+                response = await rp({
                     uri: `http://${config.hubitat.host}/apps/api/88/devices/36?access_token=${config.hubitat.access_token}`,
                     json: true
                 });
@@ -25,6 +30,41 @@ keyboard.on('keypress', async (e) => {
                 }
 
                 break;
+
+            case 4:
+                response = await rp({
+                    uri: `http://${config.hubitat.host}/apps/api/88/devices/110?access_token=${config.hubitat.access_token}`,
+                    json: true
+                });
+
+                levelAttribute = null;
+
+                if (response && response.attributes[0]?.name == 'level') {
+                    levelAttribute = response.attributes[0];
+                } else if (response && response.attributes[1]?.name == 'level') {
+                    levelAttribute = response.attributes[1];
+                }
+
+                console.log('current level', levelAttribute);
+
+                if (levelAttribute) {
+                    switch (levelAttribute.currentValue) {
+                        case 1:
+                        default:
+                            rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/50?access_token=${config.hubitat.access_token}`);
+                            break;
+
+                        case 20:
+                            rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/1?access_token=${config.hubitat.access_token}`);
+                            break;
+
+                        case 50:
+                            rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/20?access_token=${config.hubitat.access_token}`);
+                            break;
+
+                    }
+                }
+
 
         }
     }
