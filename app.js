b749ddcfa5dcb9f908ad868c04c3aaf3e69bb7ff
Bertrand Fan <bertrand@fan.net>
2023-12-27 15:04:56 -0800

    use nexus

--- app.js
+++ app.js
@@ -6,100 +6,32 @@ const keyboard = new InputEvent.Keyboard(input);
 
 console.log('zerokey started');
 
+const os = require("os");
+const hostname = os.hostname();
+
 keyboard.on('keypress', async (e) => {
     if (e.value === 1) {
 
-        let response = null;
-
-        switch (e.code) {
-            case 2:
-
-                response = await rp({
-                    uri: `http://${config.hubitat.host}/apps/api/88/devices/36?access_token=${config.hubitat.access_token}`,
-                    json: true
-                });
-
-                levelAttribute = null
-                for (let attribute of response.attributes) {
-                    if (attribute.name === 'level') {
-                        levelAttribute = attribute;
-                    }
-                }
-
-                switchAttribute = null
-                for (let attribute of response.attributes) {
-                    if (attribute.name === 'switch') {
-                        switchAttribute = attribute;
-                    }
-                }
-
-                console.log('current level', levelAttribute);
-                console.log('current switch', switchAttribute);
-
-                if (switchAttribute.currentValue == 'off') {
-                    // rp(`http://${config.hubitat.host}/apps/api/88/devices/36/on?access_token=${config.hubitat.access_token}`);
-                    rp(`http://${config.hubitat.host}/apps/api/88/devices/36/setLevel/25?access_token=${config.hubitat.access_token}`);
-                } else {
-                    // if (levelAttribute.currentValue == 25) {
-                    //     rp(`http://${config.hubitat.host}/apps/api/88/devices/36/setLevel/100?access_token=${config.hubitat.access_token}`);
-                    // } else {
-                        rp(`http://${config.hubitat.host}/apps/api/88/devices/36/off?access_token=${config.hubitat.access_token}`);
-                    // }
-                }
-
-                break;
-
-            case 3:
-                response = await rp({
-                    uri: `http://${config.hubitat.host}/apps/api/88/devices/74?access_token=${config.hubitat.access_token}`,
-                    json: true
-                });
-
-                levelAttribute = null
-                for (let attribute of response.attributes) {
-                    if (attribute.name === 'level') {
-                        levelAttribute = attribute;
-                    }
-                }
-
-                switchAttribute = null
-                for (let attribute of response.attributes) {
-                    if (attribute.name === 'switch') {
-                        switchAttribute = attribute;
-                    }
-                }
-
-                if (switchAttribute.currentValue == 'off') {
-                    console.log('open blinds');
-                    rp(`http://${config.hubitat.host}/apps/api/88/devices/74/setLevel/100?access_token=${config.hubitat.access_token}`);
-                } else {
-                    console.log('close blinds');
-                    rp(`http://${config.hubitat.host}/apps/api/88/devices/74/setLevel/0?access_token=${config.hubitat.access_token}`);
-                }
-
-                break;
-
-            case 4:
-                console.log('toggling egypt light');
-                response = await rp({
-                    uri: config.homeassistant.webhook1,
-                    method: 'POST',
-                });
-                break;
-
-            case 5:
-                console.log('setting sconce to 50%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/50?access_token=${config.hubitat.access_token}`);
-                break;
-            case 6:
-                console.log('setting sconce to 20%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/20?access_token=${config.hubitat.access_token}`);
-                break;
-            case 7:
-                console.log('setting sconce to 1%');
-                rp(`http://${config.hubitat.host}/apps/api/88/devices/110/setLevel/1?access_token=${config.hubitat.access_token}`);
-                break;
-
+        if (!config.codes[e.code.toString()]) {
+            console.log('unknown key', e.code);
+            return;
         }
+
+        key = config.codes[e.code.toString()]
+
+        body = {
+            client_id: hostname,
+            key: key
+        };
+
+        console.log(body);
+
+        response = await rp({
+            uri: `http://${config.nexus.host}/key/press`,
+            method: 'POST',
+            json: true,
+            body: body,
+        });
+
     }
 });
